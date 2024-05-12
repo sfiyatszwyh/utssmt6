b638888918fd1bf682abc6bd6a7540967107ce9c
sfiyatszwyh <sfiyatszwyh.co.id>
2024-05-12 23:27:11 +0700

    uts smt6

--- web_api/settings.py
+++ web_api/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'web_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'activity',
+        'USER' : 'root',
+        'PASSWORD' : '031002',
+        'HOST' : 'localhost',
+        'PORT' : '3306'
     }
 }
 
