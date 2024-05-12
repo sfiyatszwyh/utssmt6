aa2fc646f696e1092cd17278a996012f4231302f
sfiyatszwyh <sfiyatszwyh.co.id>
2024-05-12 23:43:48 +0700

    uts smt 6

--- activity/models.py
+++ activity/models.py
@@ -7,14 +7,12 @@ class Category(models.Model):
         return self.name
 
 class Activity(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
     def __str__(self):
-        return self.title
+        return self.category
 
 class Schedule(models.Model):
     date = models.DateField()
--- activity/serializers.py
+++ activity/serializers.py
@@ -13,9 +13,24 @@ class ActivitySerializer(serializers.ModelSerializer):
         model = Activity
         fields = '__all__'
 
+    def create(self, validated_data):
+        category_data = validated_data.pop('category')
+        category = Category.objects.create(**category_data)
+        activity = Activity.objects.create(category=category, **validated_data)
+        return activity
+
 class ScheduleSerializer(serializers.ModelSerializer):
     activities = ActivitySerializer(many=True)
 
     class Meta:
         model = Schedule
         fields = '__all__'
+
+    def create(self, validated_data):
+        activities_data = validated_data.pop('activities')
+        schedule = Schedule.objects.create(**validated_data)
+        for activity_data in activities_data:
+            category_data = activity_data.pop('category')
+            category = Category.objects.create(**category_data)
+            activity = Activity.objects.create(schedule=schedule, category=category, **activity_data)
+        return schedule
